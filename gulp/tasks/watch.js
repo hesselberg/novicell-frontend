e50d01d92d01f634440a8f99c49754d26c4a412c
Danni Larsen <dla@novicell.dk>
2015-07-16 15:47:46 +0200

    updated watch task
    maybe better?

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,19 +2,14 @@ var gulp = require('gulp');
 var config = require('../config.js');
 var plugins = require('gulp-load-plugins')();
 
-gulp.task("livereload", function () {
-    plugins.livereload.listen(config.livereloadPort);
-
-    return gulp.watch(config.livereloadPaths)
-        .on('change', plugins.livereload.changed);
-});
-
 gulp.task('watch', function () {
+    plugins.livereload.listen();
+
     gulp.watch(config.watchScripts, ["scripts"]);
     gulp.watch(config.watchStyles, ["styles"]);
     gulp.watch(config.watchImages, ["images"]);
     gulp.watch(config.watchIcons, ["icons"]);
     gulp.watch(config.watchFonts, ["fonts"]);
 
-    return gulp.start('livereload');
+    gulp.watch(config.livereloadPaths).on('change', plugins.livereload.changed);
 });
